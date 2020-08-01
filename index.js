948a06139bc152227c32df5b7dca102561906640
JaganC <jagancj@outlook.com>
2020-08-01 11:38:10 +0530

    Sound on Click

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-keyPressed  = () => {
-    console.log('KeyDown');
-};
+keyPressed = (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.keyblock[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    key.classList.add('sound');
+    audio.play();
+}
+const keyList = document.querySelectorAll('.keyblock');
+console.log(keyList);
+window.addEventListener('keydown', keyPressed);
